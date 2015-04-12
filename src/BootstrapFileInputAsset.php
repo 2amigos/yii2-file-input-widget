a59c9dc1342507b1cbc28b4fc80ecc4fbcc57627
Alexander Kochetov <creocoder@gmail.com>
2015-04-13 01:18:14 +0300

    Updated dosamigos\fileinput\BootstrapFileInputAsset

--- src/BootstrapFileInputAsset.php
+++ src/BootstrapFileInputAsset.php
@@ -1,30 +1,29 @@
 <?php
 /**
- * @copyright Copyright (c) 2013 2amigOS! Consulting Group LLC
- * @link http://2amigos.us
- * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @link https://github.com/2amigos/yii2-file-input-widget
+ * @copyright Copyright (c) 2013-2015 2amigOS! Consulting Group LLC
+ * @license http://opensource.org/licenses/BSD-3-Clause
  */
+
 namespace dosamigos\fileinput;
 
 use yii\web\AssetBundle;
 
 /**
- * @author Antonio Ramirez <amigo.cobos@gmail.com>
- * @link http://www.ramirezcobos.com/
- * @link http://www.2amigos.us/
- * @package dosamigos\yii2\widgets
+ * BootstrapFileInputAsset
+ *
+ * @author Alexander Kochetov <creocoder@gmail.com>
  */
 class BootstrapFileInputAsset extends AssetBundle
 {
     public $sourcePath = '@vendor/kartik-v/bootstrap-fileinput';
-
-    public $depends = [
-        'yii\bootstrap\BootstrapPluginAsset'
+    public $css = [
+        'css/fileinput.css',
     ];
-
-    public function init()
-    {
-        $this->css[] = YII_DEBUG ? 'css/fileinput.css' : 'css/fileinput.min.css';
-        $this->js[] = YII_DEBUG ? 'js/fileinput.js' : 'js/fileinput.min.js';
-    }
-} 
+    public $js = [
+        'js/fileinput.js',
+    ];
+    public $depends = [
+        'yii\bootstrap\BootstrapPluginAsset',
+    ];
+}
