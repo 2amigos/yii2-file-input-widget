3cc4832f719836274523fb02775cd371b7452519
Alexander Kochetov <creocoder@gmail.com>
2015-04-13 01:18:46 +0300

    Updated dosamigos\fileinput\FileInputAsset

--- src/FileInputAsset.php
+++ src/FileInputAsset.php
@@ -1,31 +1,29 @@
 <?php
 /**
- * @copyright Copyright (c) 2013 2amigOS! Consulting Group LLC
- * @link http://2amigos.us
- * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @link https://github.com/2amigos/yii2-file-input-widget
+ * @copyright Copyright (c) 2013-2015 2amigOS! Consulting Group LLC
+ * @license http://opensource.org/licenses/BSD-3-Clause
  */
+
 namespace dosamigos\fileinput;
 
 use yii\web\AssetBundle;
 
 /**
- * @author Antonio Ramirez <amigo.cobos@gmail.com>
- * @link http://www.ramirezcobos.com/
- * @link http://www.2amigos.us/
- * @package dosamigos\yii2\widgets
+ * FileInputAsset
+ *
+ * @author Alexander Kochetov <creocoder@gmail.com>
  */
 class FileInputAsset extends AssetBundle
 {
 	public $sourcePath = '@vendor/jasny/bootstrap/dist';
-
+    public $css = [
+        'css/jasny-bootstrap.css',
+    ];
+    public $js = [
+        'js/jasny-bootstrap.js',
+    ];
 	public $depends = [
-		'yii\bootstrap\BootstrapPluginAsset'
+		'yii\bootstrap\BootstrapPluginAsset',
 	];
-
-	public function init()
-	{
-		$this->js = YII_DEBUG ? ['js/jasny-bootstrap.js'] : ['js/jasny-bootstrap.min.js'];
-
-		$this->css = YII_DEBUG ? ['css/jasny-bootstrap.css'] : ['css/jasny-bootstrap.min.css'];
-	}
 }
